63f79fb6b285928b960dc13398f8d768f90810af
aoge2716 <aoge2716@gmail.com>
2025-02-07 17:17:01 +0100

    completed all iterations

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,64 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import TableHeader from "./components/TableHeader";
 import StudentCard from "./components/StudentCard";
-
 import studentsData from "./assets/students.json";
 
 function App() {
   const [students, setStudents] = useState(studentsData);
+
+  // WIHTOUT REFRACTOR  
+  // const [name,setName] =useState("");
+  // const [imgUrl,setImgUrl] =useState("");
+  // const [phone,setPhone] =useState("");
+  // const [email,setEmail] =useState("");
+  // const [year,setYear] =useState("");
+  // const [stat,setStat] =useState(false);
+  // console.log(name,imgUrl,phone,email)
+
+
+  // const handleSubmit = (event)=>{
+  //   event.preventDefault();
+  //   const studentObj ={
+  //     fullName: name,
+  //     email: email,
+  //     phone: phone,
+  //     program: "Web Dev",
+  //     image: imgUrl,
+  //     graduationYear: year,
+  //     graduated: stat
+  //   }
+
+  //   const newList = [studentObj, ...students];
+  //   setStudents(newList);
+  // }
+
+  // REFRACTORED
+  const [form, setForm] =useState({
+    fullName: "",
+    image: "",
+    phone: "",
+    email: "",
+    program: "",
+    graduationYear: "",
+    graduated: false,
+  });
+
+  const handleChange= (event)=>{
+    const {name, value, type, checked } = event.target;
+
+    setForm(val => ({
+      ...val,
+      [name]:type==="checkbox"? checked: value
+    }));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newList = [{...form}, ...students]
+
+    setStudents(newList);
+  }
 
 
   return (
@@ -15,34 +68,34 @@ function App() {
       <Navbar />
 
       {/* FORM */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <span>Add a Student</span>
         <div>
           <label>
             Full Name
-            <input name="fullName" type="text" placeholder="Full Name" />
+            <input name="fullName" type="text" placeholder="Full Name" value={form.fullName} onChange={handleChange}/>
           </label>
 
           <label>
             Profile Image
-            <input name="image" type="url" placeholder="Profile Image" />
+            <input name="image" type="url" placeholder="Profile Image" value={form.image} onChange={handleChange}/>
           </label>
 
           <label>
             Phone
-            <input name="phone" type="tel" placeholder="Phone" />
+            <input name="phone" type="tel" placeholder="Phone" value={form.phone} onChange={handleChange}/>
           </label>
 
           <label>
             Email
-            <input name="email" type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" value={form.email} onChange={handleChange}/>
           </label>
         </div>
 
         <div>
           <label>
             Program
-            <select name="program">
+            <select name="program" value={form.program} onChange={handleChange}>
               <option value="">-- None --</option>
               <option value="Web Dev">Web Dev</option>
               <option value="UXUI">UXUI</option>
@@ -60,12 +113,14 @@ function App() {
               maxLength={4}
               min={2023}
               max={2030}
+              value={form.graduationYear} 
+              onChange={handleChange}
             />
           </label>
 
           <label>
             Graduated
-            <input name="graduated" type="checkbox" />
+            <input name="graduated" type="checkbox" value={form.graduated} onChange={handleChange}/>
           </label>
 
           <button type="submit">Add Student</button>
